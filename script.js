b6f46a69746ea8a74235e3d4c95c4c14512f8945
Ravindran Antonysamy <ravindran.antonysamy@centuri.se>
2023-08-03 16:12:38 +0200

    removed unwanted

--- script.js
+++
@@ -1,9 +0,0 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var email = profile.getEmail();
-    
-    // Redirect to next page or display email
-    // Example: Redirect to next.html and pass email as query parameter
-    window.location.href = 'next.html?email=' + encodeURIComponent(email);
-  }
-  
